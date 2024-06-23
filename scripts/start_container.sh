b848db49df5ea560ee13dc3804fdb56ffae126d5
sudeep1111 <100224050+sudeep1111@users.noreply.github.com>
2024-06-23 13:02:30 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,1 +1,8 @@
-start
+#!/bin/bash
+set -e
+
+# Pull the Docker image from Docker Hub
+docker pull abhishekf5/simple-python-flask-app
+
+# Run the Docker image as a container
+docker run -d -p 5000:5000 abhishekf5/simple-python-flask-app
